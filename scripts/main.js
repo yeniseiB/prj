a1002593d788efa6bf74db94389e8435e96310f4
yeniseiB <bachyenisei@gmail.com>
2020-10-07 20:41:38 +0300

    cycle add

--- scripts/main.js
+++ scripts/main.js
@@ -8,24 +8,29 @@ const personalMovieDB = {
     privat: false
 };
 
-let lastFilm1 = prompt('Последний посмотренный фильм', '');
-let rate1 = prompt('На сколько оцените его', '');
 
-let lastFilm2 = prompt('Последний посмотренный фильм', '');
-let rate2 = prompt('На сколько оцените его', '');
-
-
-personalMovieDB.movies[lastFilm1] = rate1;
-personalMovieDB.movies[lastFilm2] = rate2;
 
 console.log(personalMovieDB);
 
-if (personalMovieDB.count[numberOfFilms] < 10) {
+if (personalMovieDB.count < 10) {
     console.log('Посмотренно довольно мало фильмов');
-} else if (personalMovieDB.count[numberOfFilms] <= 30) {
+} else if (personalMovieDB.count <= 30) {
     console.log('Вы скласический зритель');
-} else if (personalMovieDB.count[numberOfFilms] > 30) {
+} else if (personalMovieDB.count > 30) {
     console.log('ВЫ КИНОМАН');
 }  else {
     console.log('Произошла ошибка!');
+}
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Последний посмотренный фильм', '');
+    let b = prompt('На сколько оцените его', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done!');
+    } else {
+        console.log('error!');
+        i--;
+    }
 }
